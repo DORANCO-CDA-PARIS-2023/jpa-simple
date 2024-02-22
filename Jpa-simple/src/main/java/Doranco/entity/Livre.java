3914d4a4d35b2f001811ee4fea49205d7db70cdf
Ryan Sifi <ryan.sifi.77@gmail.com>
2024-02-22 12:21:18 +0100

    Avancement-2

--- Jpa-simple/src/main/java/Doranco/entity/Livre.java
+++ Jpa-simple/src/main/java/Doranco/entity/Livre.java
@@ -1,1 +1,13 @@
+import jakarta.persistence.*;
 
+@Entity
+public class Livre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titre;
+    private String auteur;
+    private String genre;
+    private int anneePublication;
+    private int nombrePages;
+}
