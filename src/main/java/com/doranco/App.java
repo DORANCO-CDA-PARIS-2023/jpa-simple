9f97518020cfd717b1d13c89c78f3d8bbf6670b3
William T. <william.tahar@gmail.com>
2024-02-22 17:04:39 +0100

    correction

---
+++ src/main/java/com/doranco/App.java
@@ -0,0 +1,33 @@
+package com.doranco;
+
+import com.doranco.entity.Livre;
+import com.doranco.service.LivreService;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try (var ef = Persistence.createEntityManagerFactory("correctionTp3")) {
+            var manager = ef.createEntityManager();
+
+            LivreService service = new LivreService(manager);
+
+            service.create(new Livre("super titre", "Bob", "SF", 1900, 100));
+            service.create(new Livre("titre2", "Bob", "SF", 1960, 100));
+            service.create(new Livre("titre 3", "titi", "Horror", 2000, 100));
+            service.create(new Livre("Titre 4", "toto", "Fantasy", 2010, 100));
+
+            List<Livre> livres = service.findByAuthor("bob");
+            for (Livre livre : livres)
+            {
+                System.out.println(livre);
+            }
+
+            manager.close();
+        }
+    }
+}
---
+++ src/main/java/com/doranco/entity/Livre.java
@@ -0,0 +1,86 @@
+package com.doranco.entity;
+
+import jakarta.persistence.*;
+
+@Entity @Table
+public class Livre {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
+    private int yearPublish;
+    private int pageNumber;
+
+
+    public Livre() {}
+
+    public Livre(String title, String author, String genre, int yearPublish, int pageNumber) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.yearPublish = yearPublish;
+        this.pageNumber = pageNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYearPublish() {
+        return yearPublish;
+    }
+
+    public void setYearPublish(int yearPublish) {
+        this.yearPublish = yearPublish;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Livre{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", yearPublish=" + yearPublish +
+                ", pageNumber=" + pageNumber +
+                '}';
+    }
+}
